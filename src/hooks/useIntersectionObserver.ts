7f60a35e80914841794bb0bc884ec0beb222e22e
choiwonseokgit <cs0328@hanmail.net>
2024-03-29 23:11:11 +0900

    Feat: SSR시 interSectionObserver 사용할 수 있게 수정 완료

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,36 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function useIntersectionObserver(): [
-  (element: HTMLElement) => void,
-  (element: HTMLElement) => void,
+  (element: HTMLElement | null) => void,
   boolean
 ] {
   const [isScrolled, setIsScrolled] = useState(false);
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  const observer = useRef<IntersectionObserver>(
-    new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsScrolled(true);
-          } else {
-            setIsScrolled(false);
-          }
-        });
-      },
-      {
-        threshold: 1,
+  useEffect(() => {
+    const handleIntersect: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        setIsScrolled(entry.isIntersecting);
+      });
+    };
+
+    observer.current = new IntersectionObserver(handleIntersect, {
+      threshold: 1,
+    });
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
       }
-    )
-  );
+    };
+  }, []);
 
-  const observe = (element: HTMLElement) => {
-    observer.current.observe(element);
+  const observe = (element: HTMLElement | null) => {
+    if (observer.current && element) {
+      observer.current.observe(element);
+    }
   };
 
-  const unobserve = (element: HTMLElement) => {
-    observer.current.unobserve(element);
-  };
-
-  return [observe, unobserve, isScrolled];
+  return [observe, isScrolled];
 }
