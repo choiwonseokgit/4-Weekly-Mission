2a5c30a9c121f0a41778aef9d3325a1d2a4b4cdf
choiwonseokgit <cs0328@hanmail.net>
2024-04-07 14:37:14 +0900

    Feat: signin, signup 페이지에는 HeaderFrame, BottomNavbar 안뜨게 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import Head from "next/head";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 import BottomNavBar from "@/components/BottomNavBar";
 import HeaderFrame from "@/components/HeaderFrame";
 import type { AppProps } from "next/app";
 
+const SIGN_PAGE = ["/signin", "/signup"];
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { asPath } = router;
+
+  if (SIGN_PAGE.includes(asPath))
+    return (
+      <>
+        <Head>
+          <title>Linkbrary</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+
   return (
     <>
       <Head>
